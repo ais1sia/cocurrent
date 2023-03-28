4bfba2e60b9780ce401317168cd542ae1aa38e03
ais1sia <242519@edu.p.lodz.pl>
2023-03-28 16:56:20 +0200

    etap0

---
+++ AplikacjaTests/MainWindowTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aplikacja;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikacja.Tests
+{
+    [TestClass()]
+    public class MainWindowTests
+    {
+        [TestMethod()]
+        public void MainWindowTest()
+        {
+            Assert.AreEqual((1+1), 2);
+            Assert.IsTrue((1+1) == 2);
+        }
+    }
+}
