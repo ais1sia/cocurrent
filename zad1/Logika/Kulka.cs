c8c7b1877584588ca29334298bb3bb72cf45a57a
ais1sia <242519@edu.p.lodz.pl>
2023-04-19 15:31:55 +0200

    bug

---
+++ zad1/Logika/Kulka.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Logika
+{
+    public class Kulka : IEquatable<Kulka>
+    {
+        public int Srednica { get; init; }
+
+        public Vector2 Szybkosc { get; set; }
+        public Vector2 Pozycja { get; set; }
+
+      
+        public Kulka(int srednica, Vector2 szybkosc, Vector2 pozycja)
+        {
+            Srednica = srednica;
+            Szybkosc = szybkosc;
+            Pozycja = pozycja;
+        }
+
+        public void Poruszanie(Vector2 granicaX, Vector2 granicaY) 
+        {
+
+            if (Szybkosc.CzyZero())    
+            {
+                return;
+            }
+
+            Pozycja += Szybkosc;// Jestem predkosciom ~ Wojtas 7.4.2023
+
+            var (pozX, pozY) = Pozycja; 
+
+            
+            if (!pozX.IsBetween(granicaX.X-25, granicaX.Y, Srednica))
+            {
+                Szybkosc = new Vector2(-Szybkosc.X, Szybkosc.Y);
+            }
+            if (!pozY.IsBetween(granicaY.X-25, granicaY.Y, Srednica))
+            {
+                Szybkosc = new Vector2(Szybkosc.X, -Szybkosc.Y);
+            }
+            
+        }
+
+        //wymagane do interfejsu equatable
+        public bool Equals(Kulka? other)
+        {
+            return other is not null
+                && Srednica == other.Srednica
+                && Pozycja == other.Pozycja
+                && Szybkosc == other.Szybkosc;
+        }
+        
+    }
+}
---
+++ zad1/Model/KulkaModel.cs
@@ -0,0 +1,18 @@
+﻿using Logika;
+
+namespace Model
+{
+    public class KulkaModel
+    {
+        private readonly Kulka kula;
+
+        public KulkaModel(Kulka _kula)
+        {
+            this.kula = _kula;
+        }
+        
+        public int Srednica => kula.Srednica;
+        public Vector2 Pozycja => kula.Pozycja;
+        public Vector2 Szybkosc => kula.Szybkosc;
+    }
+}
